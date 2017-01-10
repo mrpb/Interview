1579b41f639d862a45dfdea6e8517070ed894583
Piotr Brzezinski <piotrbrzezinski+github@outlook.com>
2017-01-10 23:36:07 +0100

    Empty project file and directory structure

---
+++ BinarySearch/Interview.BinarySearch/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.BinarySearch
+{
+    public class Class1
+    {
+    }
+}
