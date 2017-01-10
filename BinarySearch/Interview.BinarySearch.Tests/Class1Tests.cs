3b842242f0799b2f8107a95f2e18909de87737ce
Piotr Brzezinski <piotrbrzezinski+github@outlook.com>
2017-01-10 23:56:10 +0100

    Binary Search naive implementation with testing project

--- BinarySearch/Interview.BinarySearch.Tests/Class1Tests.cs
+++ BinarySearch/Interview.BinarySearch.Tests/Class1Tests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Interview.BinarySearch;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,13 @@ using System.Threading.Tasks;
 
 namespace Interview.BinarySearch.Tests
 {
-    class Class1Tests
+    [TestClass()]
+    public class Class1Tests
     {
+        [TestMethod()]
+        public void BinarySearchTest()
+        {
+            Assert.Fail();
+        }
     }
 }
