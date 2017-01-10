5d1990e4d013d9bd8bff9362307b0b4a5f7baaf5
Piotr Brzezinski <piotrbrzezinski+github@outlook.com>
2017-01-10 23:53:18 +0100

    Binary Search naive implementation with testing project

--- BinarySearch/Interview.BinarySearch/Class1.cs
+++ BinarySearch/Interview.BinarySearch/Class1.cs
@@ -8,5 +8,32 @@ namespace Interview.BinarySearch
 {
     public class Class1
     {
+        private List<int> _list;
+
+        public Class1(IEnumerable<int> values)
+        {
+            _list = new List<int>(values);
+        }
+
+        public int BinarySearch(int val)
+        {
+            int leftIdx = 0;
+            int rightIdx = _list.Count;
+
+            while(leftIdx != rightIdx)
+            {
+                int midIdx = (int)(rightIdx - leftIdx) / 2;
+                int midVal = _list[midIdx];
+
+                if (midVal == val)
+                    return midIdx;
+                else if (val < midVal)
+                    rightIdx = midIdx;
+                else
+                    leftIdx = midIdx;
+            }
+
+            return -1;
+        }
     }
 }
