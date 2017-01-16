ad6e3590ada091222f54c2d7a0627b7f9d0095fd
Piotr Brzezinski <piotrbrzezinski+github@outlook.com>
2017-01-17 00:29:14 +0100

    Simple approach with test showing non-atomic read/write operation on variable

---
+++ ParallelCounter/Interview.ParallelCounter.Tests/SimpleApproachTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Interview.ParallelCounter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Interview.ParallelCounter.Tests
+{
+    [TestClass()]
+    public class SimpleApproachTests
+    {
+        private const int iterations = 100000;
+
+        [TestMethod()]
+        public void IncrementTest_CountersAreEqual()
+        {
+            SimpleApproach counter = new SimpleApproach();
+
+            using (CountdownEvent finish = new CountdownEvent(iterations))
+            {
+                for (int i = 0; i < iterations; i++)
+                {
+                    ThreadPool.QueueUserWorkItem(x =>
+                                                    {
+                                                        ((SimpleApproach)x).Increment();
+                                                        finish.Signal();
+                                                    },
+                                                counter);
+                }
+
+                finish.Wait();
+            }
+
+            Assert.AreEqual(iterations, counter.Counter);
+        }
+    }
+}
---
+++ ParallelCounter/Interview.ParallelCounter/SimpleApproach.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.ParallelCounter
+{
+    public class SimpleApproach
+    {
+        public int Counter { get; private set; }
+
+        public void Increment()
+        {
+            Counter += 1;
+        }
+    }
+}
