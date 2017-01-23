5cb33dd6262c1528b56afa4c44bac774119ca57f
Piotr Brzezinski <piotrbrzezinski+github@outlook.com>
2017-01-23 23:56:21 +0100

    Not even a full commit

---
+++ LinkedList/Interview.LinkedList/CustomLinkedList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.LinkedList
+{
+    public class CustomLinkedList<T> : IEnumerable<CustomLinkedListNode<T>>
+    {
+        CustomLinkedListNode<T> _head;
+
+        public IEnumerator<CustomLinkedListNode<T>> GetEnumerator()
+        {
+            if (_head == null)
+                return null;
+
+            CustomLinkedListNode<T> 
+
+            while()
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CustomLinkedListNode<T>
+    {
+        public T NodeValue { get; set; }
+
+        public CustomLinkedListNode<T> Previous { get; set; }
+
+        public CustomLinkedListNode<T> Next { get; set; }
+    }
+}
---
+++ LinkedList/Interview.LinkedList/CycleFinder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.LinkedList
+{
+    public class CycleFinder
+    {
+        LinkedList<int> _list;
+
+        public CycleFinder(IEnumerable<int> collection)
+        {
+            _list = new LinkedList<int>(collection);
+        }
+
+        public bool HasCycle()
+        {
+            foreach (var item in _list)
+            {
+
+            }
+
+            return false;
+        }
+    }
+}
